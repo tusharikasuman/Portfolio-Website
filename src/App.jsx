f6a0f9bf32eba5f2ccaaae29ca0488d762505a1c
tusharikasuman <tusharika.suman6@gmail.com>
2025-04-30 22:21:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Tech from './components/Tech'
+import './index.css'
+
+
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased select-none selection:bg-cyan-300 selection:text-cyan-900"> 
+      {/* Background Blur Effect */}
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="relative h-full w-full bg-slate-950">
+          <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+          <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        </div>
+      </div>
+
+      {/* Foreground Content */}
+      <Navbar />
+      <Hero />
+      <About />
+      <Tech /> 
+      
+    </div>
+  )
+}
+
+export default App
